49659a2bd807867b4f0bc3afa6b73ec2b5dda600
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-06 17:55:37 +0530

    Create data.js and import it in app.js using require

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
+// import { userName } from './data';
+// console.log(userName) //work in javascript 
+
+let data=require('./data')
+console.log(data)
+
+//----------------------------------
 
 //require():Used to import modules, JSON, and local files.
-let fs =require('fs')//fs:file system is internal module default we don't need to install
+let fs =require('fs');//fs:file system is internal module default we don't need to install
+const { userName } = require('./data');
 fs.writeFileSync("vinayakKoli.txt","My Name Is Vinayak Koli")//filename or file descriptor
 console.log(20+20)
 
---
+++ backend/data.js
@@ -0,0 +1,1 @@
+export let userName="Vinayak Koli"
