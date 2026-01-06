5dfbeaf3435c62ee2126f8b5430e14b229ce1b37
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-06 12:58:47 +0530

    Practiced Node.js basics (fs module, require, functions, console)

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,19 @@
+// //require():Used to import modules, JSON, and local files.
+// let fs =require('fs')//fs:file system is internal module default we don't need to install
+// fs.writeFileSync("vinayakKoli.txt","My Name Is Vinayak Koli")//filename or file descriptor
+// console.log(20+20)
+
+// console.log("Hello Node.js Learner")
+
+// function fruit(item){
+// console.log("fruit name is "+item)
+// }
+// fruit("mango")
+// fruit("apple")
+
+// let a=10
+// let b=20
+// console.log("Addition -: ",a+b)
 //require():Used to import modules, JSON, and local files.
 let fs =require('fs')//fs:file system is internal module default we don't need to install
 fs.writeFileSync("vinayakKoli.txt","My Name Is Vinayak Koli")//filename or file descriptor
