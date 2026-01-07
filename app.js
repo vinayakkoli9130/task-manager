7038b29f3d2a7504e7f9ee3183f3ec0f3289c345
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-07 10:20:36 +0530

    Practice CommonJS exports and destructuring patterns

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
-// import { userName } from './data';
+// // import { userName } from './data';
 // console.log(userName) //work in javascript 
+//------------- two object in same module-----------------------------------
+ let info=require('./data')
+ console.log(info.empInfo.empId)
+  console.log(info.empInfo.empName)
+   console.log(info.empInfo.empSalary)
+ console.log(info.empAdd.currCity)
+ console.log(info.empAdd.empDist)
+ console.log(info.empAdd.empTaluka)
+ console.log(info.empAdd.villaige)
 
-let data=require('./data')
-console.log(data)
+ //Destructuring
 
-let {cityName}=require('./data')
-console.log(cityName)
-//----------------------------------
+  const {empInfo,empAdd}=require("./data")
+ console.log(empInfo.empId)
+ console.log(empInfo.empName)
+ console.log(empInfo.empSalary)
+ console.log("--------------------------------------")
+ console.log(empAdd.currCity)
+ console.log(empAdd.empDist)
+ console.log(empAdd.empTaluka)
+ console.log(empAdd.villaige)
+console.log("--------------------------------------------")
+const {empInfo:{empId,empName,empSalary},empAdd:{currCity,empDist,empTaluka,villaige}}=require("./data")
+console.log(empId)
+console.log(empName)
+console.log(empSalary)
+console.log(currCity)
+console.log(empDist)
+console.log(empTaluka)
+console.log(villaige)
+////-----------------------------------------------------------
+// let data=require('./data')
+// console.log(data)//print object of data
+// console.log(data.userName)
+// console.log(data.cityName)
+// console.log(data.age)
 
-//require():Used to import modules, JSON, and local files.
+// //object destructuring
+// let {userName,cityName,age}=require('./data')
+// console.log(userName)
+// console.log(age)
+// console.log(cityName)
+// //----------------------------------
+
+// //require():Used to import modules, JSON, and local files.
 let fs =require('fs');//fs:file system is internal module default we don't need to install
-const { userName } = require('./data');
+// const { userName } = require('./data');
 fs.writeFileSync("vinayakKoli.txt","My Name Is Vinayak Koli")//filename or file descriptor
 console.log(20+20)
 
--- data.js
+++ data.js
@@ -2,6 +2,28 @@
 // export let userName="Vinayak Koli"
 
 //commonjs:beginner and node default
+
 module.exports={
-cityName:"Pune"
+    userName:"Vinayak Koli",
+cityName:"Pune",
+age:26
+}
+
+//if we have two or more object 
+
+let empInfo={
+    empName:"Mahesh Koli",
+    empId:"vinny25",
+    empSalary:38000
+}
+let empAdd={
+    currCity:"Pune",
+    empDist:"Solapur",
+    empTaluka:"Mangalwedha",
+    villaige:"Nandur"
+}
+
+module.exports={
+    empInfo,
+    empAdd
 }
