248749f89ebbe7b917c9674ff43155b198c894f7
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-08 14:08:20 +0530

    Create basic HTTP server using Node.js core module

---
+++ server.js
@@ -0,0 +1,13 @@
+//create server using defult package http
+
+const http=require('http')//
+
+// http.createServer((req,resp)=>{ //take two parameter
+//     resp.write("This is Vinayak Koli")
+// resp.end("Hello")
+// }).listen(4800)
+
+http.createServer((req,resp)=>{ //take two parameter
+    resp.write("<h1>This is Vinayak Koli</h>");//here we can also write html,create page on server
+resp.end(" Hello ")// end responce.if we can not end responce using resp.end() requiset is pendenig
+}).listen(4800)//port
