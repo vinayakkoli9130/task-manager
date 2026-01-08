da00a2927aa7c801399fb34094e01acb8a6aee3a
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-08 15:33:06 +0530

    Add second HTTP server on different port

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ http.createServer((req,resp)=>{ //take two parameter
     resp.write("<h1>This is Vinayak Koli</h>");//here we can also write html,create page on server
 resp.end(" Hello ")// end responce.if we can not end responce using resp.end() requiset is pendenig
 }).listen(4800)//port
+
+//Run Another server in nodejs
+http.createServer((req,resp)=>{ 
+    resp.write("<h1>Another Server</h1>")
+resp.end(" Hello ")
+}).listen(5800)
