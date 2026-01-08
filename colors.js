a2a05c9c9e1028816a73f6eb5c94237091ef21b3
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-08 14:48:20 +0530

    Add colors package example using npm

---
+++ colors.js
@@ -0,0 +1,12 @@
+//use third party package/module. that install tu use using npm i colors(package name)
+const color=require("colors")
+
+console.log(color.bgGreen("Hello"))
+
+console.log(color.red("My Name Is Vinayak"))
+
+console.log(color.green("from pune"))
+
+console.log(color.america("Learning Node.js"))
+
+console.log(color.strikethrough("Make Application"))
