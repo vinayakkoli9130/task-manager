95a24d8609c7b453efce4d5f3570da4efb75fd79
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-10 18:57:03 +0530

    Fix GET and POST request handling in Node.js form server

--- form.js
+++ form.js
@@ -1,43 +1,50 @@
-const http=require('http');
+// 
+const http = require('http');
+const queryString = require('querystring');
+const fs = require("fs");
 
-const queryString=require('querystring')
-//access separate file html
-const fs=require("fs")
+http.createServer((req, resp) => {
 
-http.createServer((req,resp)=>{
+    if (req.method === "GET" && req.url === "/") {
 
-    fs.readFile("html/form.html","utf-8",(err,data)=>{
+        fs.readFile("html/form.html", "utf-8", (err, data) => {
 
-        if(err){
-             resp.writeHead(500,{"Content-Type":"text/plain"})
-            resp.end("Internal Server Error")
-             return;
-        }
+            if (err) {
+               
+                resp.writeHead(500, { "Content-Type": "text/plain" });
+                resp.end("Internal Server Error");
+                return;
+            }
 
-        resp.writeHead(200,{"Content-Type":"text/html"})
-    if(req.url=="/"){
-    resp.write(data)
+            resp.writeHead(200, { "Content-Type": "text/html" });
+            resp.end(data); // 
+        });
     }
 
-    else if(req.url=="/submit"){
-        let dataBody=[]
-        req.on('data',(chunk)=>{
-        dataBody.push(chunk)
-        })
-        resp.write("<h1>Data Is Submitted!</h1>")
+  
+    else if (req.method === "POST" && req.url === "/submit") {
 
-        req.on('end',()=>{
-            let rawData=Buffer.concat(dataBody).toString()
+        let dataBody = [];
 
-            let readableData=queryString.parse(rawData)
-            console.log(readableData)
-        })
+        req.on('data', (chunk) => {
+            dataBody.push(chunk);
+        });
+
+        req.on('end', () => {
+            let rawData = Buffer.concat(dataBody).toString();
+            let readableData = queryString.parse(rawData);
+            console.log(readableData);
+
+            //  HEADER BEFORE BODY
+            resp.writeHead(200, { 'Content-Type': 'text/html' });
+            resp.end(`
+               <h1>${readableData.name}</h1>
+               <h1>${readableData.email}</h1>
+            `);
+        });
     }
- resp.end()
-    })
 
-   
-}).listen(3200)
+}).listen(3200);
 
 //access own file html
 // http.createServer((req,resp)=>{
