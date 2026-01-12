c525145a0406cbe419c18428745a73a41f920cce
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-12 10:22:12 +0530

    modify form.js file code

--- form.js
+++ form.js
@@ -1,69 +1,105 @@
-// 
-const http = require('http');
-const queryString = require('querystring');
-const fs = require("fs");
+const http=require('http')//imort http module
+const fs=require('fs');//file system for read file
+const queryString=require('querystring') //convert data into readable form
 
-http.createServer((req, resp) => {
+http.createServer((req,resp)=>{
+fs.readFile("html/form.html","utf-8",(err,data)=>{
+if(err){
+   resp.writeHead(500,{"content-type":"text/html"})
+    resp.end("Internal Server Error")
+    return
+}
+ resp.writeHead(200,{"content-type":"text/html"})
+if(req.url=="/"){
+resp.write(data)
+}
+else if(req.url=="/submit"){
 
-    if (req.method === "GET" && req.url === "/") {
+let dataBody=[]
+//get data
+req.on('data',(chunk)=>{
+dataBody.push(chunk)
+})
 
-        fs.readFile("html/form.html", "utf-8", (err, data) => {
+ //finalise data
 
-            if (err) {
-               
-                resp.writeHead(500, { "Content-Type": "text/plain" });
-                resp.end("Internal Server Error");
-                return;
-            }
+ req.on('end',()=>{
+    let rawData=Buffer.concat(dataBody).toString()//contact and merge in to string
+ let readableData=queryString.parse(rawData)
+ console.log(readableData)
+ })
 
-            resp.writeHead(200, { "Content-Type": "text/html" });
-            resp.end(data); // 
-        });
-    }
+    resp.write("<h1>Data Is Submited</h1>")  
+}
 
-  
-    else if (req.method === "POST" && req.url === "/submit") {
+resp.end()
+})
 
-        let dataBody = [];
+}).listen(4500)
 
-        req.on('data', (chunk) => {
-            dataBody.push(chunk);
-        });
-
-        req.on('end', () => {
-            let rawData = Buffer.concat(dataBody).toString();
-            let readableData = queryString.parse(rawData);
-            console.log(readableData);
-
-            //  HEADER BEFORE BODY
-            resp.writeHead(200, { 'Content-Type': 'text/html' });
-            resp.end(`
-               <h1>${readableData.name}</h1>
-               <h1>${readableData.email}</h1>
-            `);
-        });
-    }
-
-}).listen(3200);
-
-//access own file html
-// http.createServer((req,resp)=>{
-//     console.log(req.url)
-//     resp.writeHead(200,{"Content-Type":"text/html"})
-//     if(req.url=="/"){
-//     resp.write(`
-//         <form action="/submit" method="post">
+console.log("--------------------------------------------------------------------")
+// http.createServer((req,resp)=>{//create server
+//     resp.writeHead(200,{"content-type":"text/html"})
+// if(req.url=="/"){
+// resp.write(`
+//        <form action="/submit" method="post">
 //         <input type="text" placeholder="enter name" name="name"/>
 //         <input type="email" placeholder="enter email" name="email"/>
 //         <button>Submit</button>
 //         </form>
 //         `)
-//     }
 
-//     else if(req.url=="/submit"){
-//         resp.write("<h1>Data Is Submitted!</h1>")
-//     }
-    
-// resp.end()
-// }).listen(3200)
+// }
+// else if(req.url=="/submit"){
+//     resp.write("<h1>Data Is Submited</h1>")
+   
+// }
+//   resp.end("Response End!")   
+// }).listen(1300)//port address
 
+// const http = require('http'); // import http module
+// const fs = require('fs');    // file system
+// const queryString = require('querystring'); // parse form data
+
+// http.createServer((req, resp) => {
+
+//     fs.readFile("html/form.html", "utf-8", (err, data) => {
+
+//         if (err) {
+//             resp.writeHead(500, { "Content-Type": "text/html" });
+//             resp.end("Internal Server Error");
+//             return;
+//         }
+
+//         if (req.url == "/") {
+//             resp.writeHead(200, { "Content-Type": "text/html" });
+//             resp.end(data);
+//         }
+
+//         else if (req.url == "/submit") {
+
+//             let dataBody = [];
+
+//             req.on('data', (chunk) => {
+//                 dataBody.push(chunk);
+//             });
+
+//             req.on('end', () => {
+//                 let rawData = Buffer.concat(dataBody).toString();
+//                 let readableData = queryString.parse(rawData);
+//                 console.log(readableData);
+
+//                 // ✅ SHOW DATA IN BROWSER
+//                 resp.writeHead(200, { "Content-Type": "text/html" });
+//                 resp.end(`
+//                     <h1>Data Is Submitted</h1>
+//                     <p>Name: ${readableData.name}</p>
+//                     <p>Email: ${readableData.email}</p>
+//                     <a href="/">Go Back</a>
+//                 `);
+//             });
+//         }
+
+//     });
+
+// }).listen(4500);
